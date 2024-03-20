b172b5852300e7e81d80e6fdad07f710ec85b9c7
Matheus Sarmento <matheussarmento02@gmail.com>
2024-03-20 16:08:07 -0300

    add form (sem css)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatIconModule,
+    MatRadioModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,4 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public teste = '';
+  public teste2 = false;
+
+  public periodos = [
+    {
+      tipo: 'Manhã',
+      horario: '06:00 às 12:00',
+      value: 'manha',
+    },
+    {
+      tipo: 'Tarde',
+      horario: '12:01 às 18:00',
+      value: 'tarde',
+    },
+    {
+      tipo: 'Noite',
+      horario: '18:01 às 23:00',
+      value: 'noite',
+    },
+  ];
+}
